5b737f4431681bbc2a4495cfe1dbf82cddc86c8e
BruceHYX <1205127605@qq.com>
2019-06-08 22:27:25 +0800

    2222

--- tscene.h
+++ tscene.h
@@ -179,7 +179,7 @@ private:
     void drawWave();
     void drawHP();
     void drawPlayerGold();
-    //void doGameOver();
+    //void doGameOver(); //在基类中实现
     void preLoadWavesInfo();
 
 
@@ -221,8 +221,11 @@ class hardScene : public tScene
 public:
     explicit hardScene(QWidget* parent = 0);
     ~hardScene();
+    void preLoadWavesInfo();
 protected:
-    //void keyPressEvent(QKeyEvent *event); //僵尸的出现
+    //void keyPressEvent(QKeyEvent *event); //6-8 暂时无用
+    void mousePressEvent(QMouseEvent *);  // 6-8 new
+    void paintEvent(QPaintEvent *);// 6-8 new
 private:
     QMovie* background = new QMovie(":/GameMap/hardMap2.jpg");
     QPushButton* exit = new QPushButton(this);
@@ -230,9 +233,45 @@ private:
     void uiSetup(); //在PVZ中用来设计僵尸的出现等
     QMovie* station = new QMovie("/image/open_spot.png");
     QLabel* btn1 = new QLabel(this);
+
+private:
+    void loadTowerPositions(); //√
+    void addWayPoints();
+    bool loadWave();
+    bool canBuyTower() const; //√
+    void drawWave();
+    void drawHP();
+    void drawPlayerGold();
+    //void doGameOver(); //在基类中实现
+
+private:
+    //MainWindow *		ui;
+    //int						m_waves;
+    //int						m_playerHp;
+    //int						m_playerGold;
+    //bool					m_gameEnded;
+    //bool					m_gameWin;
+    //AudioPlayer *			m_audioPlayer;
+    QList<QVariant>			m_wavesInfo;
+    QList<TowerPosition>	m_towerPositionsList; //√
+    //QList<Tower *>			m_towersList; //√
+    QList<WayPoint *>		m_wayPointsList;
+    QList<WayPoint *>       normalWayPointsList;
+    //QList<Enemy *>			m_enemyList;
+    //QList<Bullet *>			m_bulletList;
+
+
 private slots:
-    //void onTimer(); //在PVZ中用于设置阳光
-    void leave();
+    void onTimer();
+    void leave(); // emit toTitle();
+
+    //增加代码 6-6
+    void back();
+    //void startEasy();
+    //void startHard();
+
+    void updateMap(); //原来的槽
+    void gameStart(); //原来的槽
 };
 
 #endif // TSCENE_H
